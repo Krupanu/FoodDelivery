ff55afb6c3cdbaace2faf4529b487860098b8f09
”Krupanu” <Krupanu2@gmail.com>
2024-12-23 22:43:09 +0300

    Alex version

--- src/main/java/com/example/fooddelivery/service/OrderService.java
+++ src/main/java/com/example/fooddelivery/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.service;
 
+import com.example.fooddelivery.model.Food;
 import com.example.fooddelivery.model.Order;
 
 import java.util.List;
@@ -7,5 +8,6 @@ import java.util.List;
 public interface OrderService {
     Order getOrderInfo(Long id);
     List<Order> orders();
+    Order addOrder(Order order, Long currentUserId, List<Food> foods);
 
 }
--- src/main/java/com/example/fooddelivery/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/fooddelivery/service/impl/OrderServiceImpl.java
@@ -1,19 +1,28 @@
 package com.example.fooddelivery.service.impl;
 
+import com.example.fooddelivery.model.Food;
 import com.example.fooddelivery.model.Order;
+import com.example.fooddelivery.model.User;
+import com.example.fooddelivery.repository.FoodRepository;
 import com.example.fooddelivery.repository.OrderRepository;
+import com.example.fooddelivery.repository.UserRepository;
 import com.example.fooddelivery.service.OrderService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private OrderRepository orderRepository;
+    private UserRepository userRepository;
+    private FoodRepository foodRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, UserRepository userRepository, FoodRepository foodRepository) {
         this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+        this.foodRepository = foodRepository;
     }
 
     @Override
@@ -24,5 +33,15 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public List<Order> orders() {
         return orderRepository.findAll();
+    }
+
+    @Override
+    public Order addOrder(Order order, Long currentUserId, List<Food> foods) {
+        Optional<User> currentUser = userRepository.findById(currentUserId);
+        currentUser.ifPresent(order::setCreatedBy);
+
+        orderRepository.save(order);
+
+        return order;
     }
 }
